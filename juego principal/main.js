6c367d62ad88ba1d55566b5629004f0614426e60
Tobias Chouela <50159086@est.ort.edu.ar>
2025-08-12 14:24:27 -0300

    fgh

---
+++ juego principal/main.js
@@ -0,0 +1,9 @@
+// hice todas las varientes que se pero de ahi nada mas preguntale a ivo como hacemos
+let atacar = document.getElementById ("Atacar")
+let reagrupar = document.getElementById("Reagrupar")
+let reponer = document.getElementById ("Reponer")
+let canjear = document.getElementById ("Canjear")
+let menu = document.getElementById ("Menu")
+let parar = document.getElementById ("Parar de atacar")
+let terminar = document.getElementById ("Terminar turno")
+let temporizador = document.getElementById ("temporizador")
