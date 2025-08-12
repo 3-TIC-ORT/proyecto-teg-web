d8bd7fc5053a99e6a31130b4bb91c0578d9ed870
Uriel Meta <you@example.com>
2025-08-12 15:51:53 -0300

    a

--- Objetivos secretos/main.js
+++ Objetivos secretos/main.js
@@ -1,24 +1,25 @@
 function elegircolorjugador (colorjugador) {
-    return Math.random() * (max - min) + min;
+    return Math.random() * (1, 6);
 }
 let colorjugador = (1, 6)
-if (colorjugador === 1) {
-    colorjugador = "ROJO"
+let colorjugador1 = elegircolorjugador 
+if (colorjugador1 === 1) {
+    colorjugador1 = "ROJO"
 }
-if (colorjugador === 2) {
-    colorjugador = "AZUL"
+if (colorjugador1 === 2) {
+    colorjugador1 = "AZUL"
 }
-if (colorjugador === 3) {
-    colorjugador = "VERDE"
+if (colorjugador1 === 3) {
+    colorjugador1 = "VERDE"
 }
-if (colorjugador === 4) {
-    colorjugador = "AMARILLO"
+if (colorjugador1 === 4) {
+    colorjugador1 = "AMARILLO"
 }
-if (colorjugador === 5) {
-    colorjugador = "ROSA"
+if (colorjugador1 === 5) {
+    colorjugador1 = "ROSA"
 }
-if (colorjugador === 6) {
-    colorjugador = "NEGRO"
+if (colorjugador1 === 6) {
+    colorjugador1 = "NEGRO"
 }
 let objetivossectretos = "TODOS LOS JUGADORES CIERRAN LOS OJOS EXCEPTO EL JUGADOR " + colorjugador +  " CUANDO TODOS CIERREN LOS OJOS, EL JUGADOR AZUL DEBE APRETAR EL BOTÓN DE REVELAR OBJETIVO SECRETO"
 console.log ()
