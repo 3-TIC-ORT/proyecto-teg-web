0ce3759d7adc1848690d71d73e88bb8d448f7796
Tobias Chouela <50159086@est.ort.edu.ar>
2025-08-13 10:40:48 -0300

    edrfg

--- juego principal/main.js
+++ juego principal/main.js
@@ -6,4 +6,31 @@ let canjear = document.getElementById ("Canjear")
 let menu = document.getElementById ("Menu")
 let parar = document.getElementById ("Parar de atacar")
 let terminar = document.getElementById ("Terminar turno")
+// timer
 let temporizador = document.getElementById ("temporizador")
+let minutesEl = document.getElementById('minutos');
+let secondsEl = document.getElementById('segundos');
+
+let newYear = '1 jan 2026'; // Cambia esta fecha por tu fecha objetivo
+
+function countdown() {
+  let newYearDate = new Date(newYear);
+  let currentDate = new Date();
+
+  let totalSeconds = (newYearDate - currentDate) / 1000;
+
+  let minutes = Math.floor(totalSeconds / 60) % 60;
+  let seconds = Math.floor(totalSeconds) % 60;
+
+
+  minutesEl.innerHTML = formatTime(minutes);
+  secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+  return time < 10 ? (`0${time}`) : time;
+}
+countdown();
+
+setInterval(countdown, 1000);
+//el timer  preguntale a ivo como lo termino
---
+++ menu principal/main.js
@@ -0,0 +1,4 @@
+let salir = document.getElementById ("salir")
+let cargar = document.getElementById ("cargar")
+let nueva = document.getElementById ("nueva")
+
