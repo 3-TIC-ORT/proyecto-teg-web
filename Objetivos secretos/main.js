d3af1a3733ee73af45b8801ca6d28753f020baf8
Uriel Meta <you@example.com>
2025-08-19 15:42:30 -0300

    estoy haciendo que se reveln los objetivos

---
+++ Objetivos secretos/main.js
@@ -0,0 +1,6 @@
+let revelar = document.getElementById("revelar")
+let objetivorevelado = document.getElementById("objetivo")
+function objetivo (){
+  objetivorevelado.textContent = "Objetivo revelado"
+}
+revelar.addEventListener ("click", objetivo)
