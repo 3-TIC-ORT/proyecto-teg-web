04489a1099f4f0b0cb5532f59e044d9d1dc6ea51
Uriel Meta <you@example.com>
2025-08-19 15:51:09 -0300

    hay que hacer que no se pueda apretar el boton de siguiente jugador si todavia no se apreto el de revelar objetivo

--- Objetivos secretos/main.js
+++ Objetivos secretos/main.js
@@ -1,6 +1,13 @@
+let siguiente = document.getElementById("siguiente")
 let revelar = document.getElementById("revelar")
 let objetivorevelado = document.getElementById("objetivo")
-function objetivo (){
-  objetivorevelado.textContent = "Objetivo revelado"
+
+function texto1 (){
+  objetivorevelado.textContent = "Objetivo revelado."
 }
-revelar.addEventListener ("click", objetivo)
+revelar.addEventListener ("click", texto1) 
+
+function texto2 (){
+    objetivorevelado.textContent = "Todos los jugadores salvo el jugador colorjugador."
+  }
+  siguiente.addEventListener ("click", texto2)
