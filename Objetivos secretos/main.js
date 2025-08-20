0b52a58a56fc934ea68030ae47c91b1a56c4e206
Uriel Meta <you@example.com>
2025-08-20 11:50:16 -0300

    Arrgele lo de que se podía apretar el botón de siguiente jugador sin antes apretar el de revelar objetivo

--- Objetivos secretos/main.js
+++ Objetivos secretos/main.js
@@ -4,10 +4,12 @@ let objetivorevelado = document.getElementById("objetivo")
 
 function texto1 (){
   objetivorevelado.textContent = "Objetivo revelado."
+  siguiente.disabled = false
 }
 revelar.addEventListener ("click", texto1) 
 
 function texto2 (){
     objetivorevelado.textContent = "Todos los jugadores salvo el jugador colorjugador."
+    siguiente.disabled = true
   }
-  siguiente.addEventListener ("click", texto2)
+  siguiente.addEventListener ("click", texto2) 
