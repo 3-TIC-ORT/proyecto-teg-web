2765debe82a57d8e4e5d5db6815358d037fff032
Tobias Chouela <50159086@est.ort.edu.ar>
2025-08-26 14:28:56 -0300

    empece la maquina de estados y estoy haciendo los div de los paises para alterarlos en css (no termine)

--- juego principal/juegoprincipal.js
+++ juego principal/juegoprincipal.js
@@ -5,6 +5,11 @@ let canjear = document.getElementById ("Canjear")
 let menu = document.getElementById ("Menu")
 let parar = document.getElementById ("Parar de atacar")
 let terminar = document.getElementById ("Terminar turno")
+let paisselecionado = document.getElementById("paisselecionado")
+let colorjugador = document.getElementById("colorjugador")
+let cantidadfichas = document.getElementById("fichas")
+let atacado= document.getElementById("paisatacado")
+let atacador = document.getElementById ("paisatacante")
 // timer
 const targetDate = new Date();
 targetDate.setSeconds(targetDate.getSeconds() + 120); 
@@ -32,9 +37,20 @@ const resultado1 = tirarDado();
 let tirar1 = document.getElementById("tirar1") 
 tirar1.addEventListener("click", tirarDado) 
 function tirarDado() {
-  let numeroAleatorio = Math.round(Math.random() * 5) + 1;
+  let numeroAleatorio = Math.round((Math.random() * 5) + 1)*3;
   console.log (numeroAleatorio) 
 }
 let tirar2 = document.getElementById("tirar2") 
 tirar2.addEventListener("click", tirarDado) 
 //esta es la función de tirar dados y también hice que se ejecute cuando apretamos los botones de tirar. 
+//maquina de estados (ataque)
+const updateOrderStatus = (event) => {
+if (tirar1 > tirar2 && atacar === paisselecionado){
+  div.style.color (paisselecionado) = colorjugador
+  console.log("ganaste")
+}
+if (tirar1 < tirar2 && atacar === paisselecionado){
+  paisselecionado = paisselecionado - fichas(paisatacado)
+  console.log ("perdiste")
+}
+}
