8640b4d07de5e10c3f3c7085982aff249c413dd8
Uriel Meta <you@example.com>
2025-09-03 15:52:38 -0300

    intentando hacer la puta maquina

--- maquinaestados.js
+++ maquinaestados.js
@@ -1,70 +1,64 @@
 let cantidadJugadores = prompt("¿Cuántos jugadores?");
 cantidadJugadores = Math.max(3, Math.min(6, parseInt(cantidadJugadores) || 3))
-const faseActual = document.getElementById('atacar');
+const faseActual = document.getElementById('faseActual');
 const botonPararAtacar = document.getElementById('botonPararAtacar');
-botonPararAtacar.addEventListener("click")
 const botonTerminarTurno = document.getElementById("terminarTurno")
 
 class fasesMachine {
-constructor() {
-this.state = 'fase de ataque';
+    constructor() {
+        this.state = 'fase de ataque';
+    }
+
+    transition(event) {
+        switch (this.state) {
+            case 'fase de ataque':
+                if (event === 'click') {
+                    console.log('Cambio de fase de ataque a fase de reagrupacion');
+                    this.state = 'fase de reagrupación';
+                }
+                break;
+            case 'fase de reagrupacion':
+                if (event === 'click') {
+                    console.log('Cambio de fase de reagrupacion a fase de reposición');
+                    this.state = 'fase de reposición';
+                }
+                break;
+            case 'fase de reposición':
+                if (event === 'click') {
+                    console.log('Cambio de fase de reposición a fase de ataque');
+                    this.state = 'fase de ataque';
+                }
+                break;
+            default:
+                console.log(`No se puede manejar el evento "${event}" en el estado "${this.state}".`);
+        }
+        return this.state;
+    }
 }
 
-transition(event) {
-switch (this.state) {
-case 'fase de ataque':
-if (event === 'press') {
-console.log('Cambio de fase de ataque a fase de reagrupacion');
-this.state = 'fase de ataque';
-}
-break;
-case 'fase de reagrupacion':
-if (event === 'press') {
-console.log('Cambio de fase de reagrupacion a fase de reposición');
-this.state = 'fase de reposición';
-}
-break;
-case 'fase de reposición':
-if (event === 'press') {
-console.log('Cambio de fase de reposición a fase de ataque');
-this.state = 'fase de ataque';
-}
-break;
-default:
-console.log(`No se puede manejar el evento "${event}" en el estado "${this.state}".`);
-}
-return this.state;
-}
+function pararAtaque() {
+
 }
 
+function terminarTurno() {
 
-for ( cantidadFases = 0 ; cantidadFases <= cantidadJugadores ; cantidadFases++) {
-cantidadFases = cantidadFases + 1
-const fases = new fasesMachine();
-const fasesTurnos = cantidadJugadores * 2
-const fasesJugadas = 0
-
-if (botonPararAtacar.click) {
-
-console.log(`Estado inicial: ${fases.state}`); 
-
-fases.transition('press');
-console.log(`Estado actual: ${fases.state}`); 
-
-fasesJugadas = fasesJugadas + 1
-
-fases.transition('press');
-console.log(`Estado actual: ${fases.state}`); 
-
-fasesJugadas = fasesJugadas + 1
 }
 
-if (cantidadFases <= cantidadJugadores) {
-cantidadFases = 0
-const reposicionesHechas = 0
-while (reposicionesHechas <= cantidadJugadores)
-fases.transition('press');
-console.log(`Estado actual: ${fases.state}`); 
-reposicionesHechas = reposicionesHechas + 1
-}
+for (cantidadFases = 0; cantidadFases <= cantidadJugadores; cantidadFases++) {
+    cantidadFases = cantidadFases + 1
+    const fases = new fasesMachine();
+
+    if (botonPararAtacar.click) {
+        fases.transition('click');
+        console.log(`fase actual: ${fases.state}`);
+    }
+
+    if (cantidadFases <= cantidadJugadores) {
+        cantidadFases = 0
+        const reposicionesHechas = 0
+        while (reposicionesHechas <= cantidadJugadores)
+            fases.transition('click');
+        console.log(`Estado actual: ${fases.state}`);
+        reposicionesHechas = reposicionesHechas + 1
+    }
 }
