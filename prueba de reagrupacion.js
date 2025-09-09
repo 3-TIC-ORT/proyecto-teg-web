6f96197a741ce1735f45b0dffb54152f3edcf40c
Tobias Chouela <50159086@est.ort.edu.ar>
2025-09-09 14:26:15 -0300

    fases de reagrupacion no exitosa

--- prueba de reagrupacion.js
+++ prueba de reagrupacion.js
@@ -1,19 +1,64 @@
 let numerofichas = document.getElementById ("numerofichas")
-let boton = document.getElementById ("button")
+let reagrupar = document.getElementById ("button")
 let numero = 1;
-let fichas =[
-    {fichas ="5", color =cantidadjugadores}
+let i =0
+let country =
+    {
+        argentina:1,
+        chile:1,
+        uruguay:1,
+        brasil:1,
+        peru:1,
+        colombia:1,
 
-]
-fichas="5+3"
-function  cantidadpasable(){
-    document.getElementById ("elegido").textContent = numerofichas.value;
-}
-while (numero >= numerofichas) {
-    let pizzeria=document.createElement ("option")
- pizzeria.textContent= "queres pasar " + numero + "fichas"
- numerofichas.appendChild (pizzeria)
- numero++
-}
+        Islandia:1,
+        Bretaña:1,
+        España:1,
+        Francia:1,
+        Alemania:1,
+        Italia:1,
+        Polonia:1,
+        Rusia:1,
 
-boton.addEventListener("click",cantidadpasable)
+        Australia:1,
+        Sumatra:1,
+        Java:1,
+        borneo:1,
+
+        sahara:1,
+        Egipto:1,
+        Etiopía:1,
+        Zaire:1,
+        Sudáfrica:1,
+        Madagascar:1,
+
+        Alaska:1,
+        Canadá:1,
+        Groenlandia:1,
+        terranova:1,
+        México:1,
+        California:1,
+        Labrador:1,
+        oregon:1,
+        nuevayork:1,
+        yukon:1,
+
+        china:1,
+        japon:1,
+        iran:1,
+        israel:1,
+        arabia:1,
+        turquia:1,
+        tartaria:1,
+        kamchatka:1,
+        taimir:1,
+        gobi:1,
+        india:1,
+        malasia:1,
+        siberria:1,
+        aral:1,
+        tartaria:1,
+        mongolia:1,
+    }
+
+
