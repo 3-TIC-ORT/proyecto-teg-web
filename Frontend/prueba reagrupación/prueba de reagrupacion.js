7013eb34617fb940e32f5f567e8b2393f0a234fa
Uriel Meta <you@example.com>
2025-09-10 15:44:11 -0300

    Me puse a hacer lo de reagrupar porque choue es muy malo (mentira choue te queremos mucho un dia pone casa que esta re piola tu casa y los budines que cocina tu mama)

---
+++ Frontend/prueba reagrupación/prueba de reagrupacion.js
@@ -0,0 +1,126 @@
+//Lo que hay que hacer para mí es que cada pais INDIVIDUALMENTE sea una variable. 
+//En el juego principal, cada país va a ser un objeto. 
+//La función para reagrupar lo que tendría que hacer es tener 3 inputs: 
+//uno que sea el país que va a dar, otro que los va a recibir y la cantidad de ejercitos que se va a mover.
+//Lo que tendría que hacer la función es restarle la cantidad de ejércitos deseada por el jugador 
+//al país que va a dar los ejércitos y sumarle esa cantidad al país que los va a recibir.
+let numerofichas = document.getElementById ("numerofichas")
+let reagrupar = document.getElementById ("reagrupar")
+let numero = 1;
+let indice =0
+
+let argentina = document.getElementById("argentina");
+let chile = document.getElementById("chile");
+let uruguay = document.getElementById("uruguay");
+let brasil = document.getElementById("brasil");
+let peru = document.getElementById("peru");
+let colombia = document.getElementById("colombia");
+
+let islandia = document.getElementById("islandia");
+let bretana = document.getElementById("bretana");
+let espana = document.getElementById("españa");
+let francia = document.getElementById("francia");
+let alemania = document.getElementById("alemania");
+let italia = document.getElementById("italia");
+let polonia = document.getElementById("polonia");
+let rusia = document.getElementById("rusia");
+
+let australia = document.getElementById("australia");
+let sumatra = document.getElementById("sumatra");
+let java = document.getElementById("java");
+let borneo = document.getElementById("borneo");
+
+let sahara = document.getElementById("sahara");
+let egipto = document.getElementById("egipto");
+let etiopia = document.getElementById("etiopia");
+let zaire = document.getElementById("zaire");
+let sudafrica = document.getElementById("sudafrica");
+let madagascar = document.getElementById("madagascar");
+
+let alaska = document.getElementById("alaska");
+let canada = document.getElementById("canada");
+let groenlandia = document.getElementById("groenlandia");
+let terranova = document.getElementById("terranova");
+let mexico = document.getElementById("mexico");
+let california = document.getElementById("california");
+let labrador = document.getElementById("labrador");
+let oregon = document.getElementById("oregon");
+let nuevaYork = document.getElementById("nuevaYork");
+let yukon = document.getElementById("yukon");
+
+let china = document.getElementById("china");
+let japon = document.getElementById("japon");
+let iran = document.getElementById("iran");
+let israel = document.getElementById("israel");
+let arabia = document.getElementById("arabia");
+let turquia = document.getElementById("turquia");
+let tartaria = document.getElementById("tartaria");
+let kamchatka = document.getElementById("kamchatka");
+let taimir = document.getElementById("taimir");
+let gobi = document.getElementById("gobi");
+let india = document.getElementById("india");
+let malasia = document.getElementById("malasia");
+let siberia = document.getElementById("siberia");
+let aral = document.getElementById("aral");
+let mongolia = document.getElementById("mongolia");
+
+let fichasargentina = 1;
+let fichaschile = 1;
+let fichasuruguay = 1;
+let fichasbrasil = 1;
+let fichasperu = 1;
+let fichascolombia = 1;
+
+let fichasislandia = 1;
+let fichasbretana = 1;
+let fichasespana = 1;
+let fichasfrancia = 1;
+let fichasalemania = 1;
+let fichasitalia = 1;
+let fichaspolonia = 1;
+let fichasrusia = 1;
+
+let fichasaustralia = 1;
+let fichassumatra = 1;
+let fichasjava = 1;
+let fichasborneo = 1;
+
+let fichassahara = 1;
+let fichasegipto = 1;
+let fichasetiopia = 1;
+let fichaszaire = 1;
+let fichassudafrica = 1;
+let fichasmadagascar = 1;
+
+let fichasalaska = 1;
+let fichascanada = 1;
+let fichasgroenlandia = 1;
+let fichasterranova = 1;
+let fichasmexico = 1;
+let fichascalifornia = 1;
+let fichaslabrador = 1;
+let fichasoregon = 1;
+let fichasnuevaYork = 1;
+let fichasyukon = 1;
+
+let fichaschina = 1;
+let fichasjapon = 1;
+let fichasiran = 1;
+let fichasisrael = 1;
+let fichasarabia = 1;
+let fichasturquia = 1;
+let fichastartaria = 1;
+let fichaskamchatka = 1;
+let fichastaimir = 1;
+let fichasgobi = 1;
+let fichasindia = 1;
+let fichasmalasia = 1;
+let fichassiberia = 1;
+let fichasaral = 1;
+let fichasmongolia = 1;
+
+paisEmisor = 1
+paisReceptor = 1
+function reagrupar (paisEmisor, paisReceptor) {
+
+}
