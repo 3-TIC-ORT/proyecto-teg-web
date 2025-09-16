b2903bcb6d0c1e85b25bbb95481b8792d44d1cc9
Tobias Chouela <50159086@est.ort.edu.ar>
2025-09-16 14:08:33 -0300

    cambios en el juego principal (cambie de lugar la maquina de estados)

--- Frontend/juego principal/juegoprincipal.js
+++ Frontend/juego principal/juegoprincipal.js
@@ -7,12 +7,81 @@ let parar = document.getElementById ("Parar de atacar")
 let terminar = document.getElementById ("Terminar turno")
 let paisselecionado = document.getElementById("paisselecionado")
 let jugador1 = document.getElementById ("")
-//let cantidadJugadores = prompt("¿Cuántos jugadores?");
+// timer
+const targetDate = new Date();
+targetDate.setSeconds(targetDate.getSeconds() + 120); 
+
+function updateTimer() {
+  const now = new Date();
+  const diff = targetDate - now;
+
+  if (diff <= 0) {
+    document.getElementById("timer").textContent = "¡Tiempo terminado!";
+    clearInterval(timerInterval);
+    return;
+  }
+  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  document.getElementById("timer").textContent =
+    String(minutes).padStart(2, '0') + ":" +
+    String(seconds).padStart(2, '0');
+
+}
+const timerInterval = setInterval(updateTimer, 1000);
+//termine el  timer
+const resultado1 = tirarDado(); 
+let tirar1 = document.getElementById("tirar1") 
+tirar1.addEventListener("click", tirarDado) 
+function tirarDado() {
+  let numeroAleatorio = Math.round((Math.random() * 5) + 1)*3;
+  console.log (numeroAleatorio) 
+}
+let tirar2 = document.getElementById("tirar2") 
+tirar2.addEventListener("click", tirarDado) 
+//esta es la función de tirar dados y también hice que se ejecute cuando apretamos los botones de tirar. 
+//cantidad de jugadores
+let botoncantidadjugadores = document.getElementById ("botoncantdadjugadores")
+let numerodejugadores =document.getElementById ("numerodejugadores")
+let numero = []
+function nombre (){
+  numero.push (numerodejugadores.value)
+  console.log (numero)
+}
+//colores 
+let objetivos =document.getElementById ("objetivos")
+let botoncolor = document.getElementById ("botoncolor")
+let colorines =document.getElementById ("color de jugadores")
+let colores = []
+function objetos (){
+
+  if (colores > cantidadJugadores || colores  <cantidadJugadores){
+    alert ("no coincide la cantidad de jugadores con los colores")
+  }
+  colores.push (objetivos.value)
+  console.log (colores)
+  
+}
+function final (){
+  while (i < players){
+  if (jugador === players[i]){
+      let pizzeria=document.createElement ("botoncolor")
+      pizzeria.textContent= players
+      objetivos.appendChild (p)}
+  i++
+
+ }
+}
+colorines.addEventListener ("click",final)
+
+//intento de la eleccion de color
+//intento color2
+// La Maquina de Estados Finitos
+let cantidadJugadores = prompt("¿Cuántos jugadores?");
 cantidadJugadores = Math.max(3, Math.min(6, parseInt(cantidadJugadores) || 3))
 const faseActual = document.getElementById('faseActual');
 const botonPararAtacar = document.getElementById('botonPararAtacar');
 const botonTerminarTurno = document.getElementById("terminarTurno")
-// La Maquina de Estados Finitos
 class fasesMachine {
         constructor() {
             this.state = 'fase de ataque';
@@ -93,72 +162,3 @@ function terminarTurno() {
 }
 botonTerminarTurno.addEventListener('click', terminarTurno);
 
-// timer
-const targetDate = new Date();
-targetDate.setSeconds(targetDate.getSeconds() + 180); 
-
-function updateTimer() {
-  const now = new Date();
-  const diff = targetDate - now;
-
-  if (diff <= 0) {
-    document.getElementById("timer").textContent = "¡Tiempo terminado!";
-    clearInterval(timerInterval);
-    return;
-  }
-  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((diff % (1000 * 60)) / 1000);
-
-  document.getElementById("timer").textContent =
-    String(minutes).padStart(2, '0') + ":" +
-    String(seconds).padStart(2, '0');
-
-}
-const timerInterval = setInterval(updateTimer, 1000);
-//termine el  timer
-const resultado1 = tirarDado(); 
-let tirar1 = document.getElementById("tirar1") 
-tirar1.addEventListener("click", tirarDado) 
-function tirarDado() {
-  let numeroAleatorio = Math.round((Math.random() * 5) + 1)*3;
-  console.log (numeroAleatorio) 
-}
-let tirar2 = document.getElementById("tirar2") 
-tirar2.addEventListener("click", tirarDado) 
-//esta es la función de tirar dados y también hice que se ejecute cuando apretamos los botones de tirar. 
-//cantidad de jugadores
-let botoncantidadjugadores = document.getElementById ("botoncantdadjugadores")
-let numerodejugadores =document.getElementById ("numerodejugadores")
-let numero = []
-function nombre (){
-  numero.push (numerodejugadores.value)
-  console.log (numero)
-}
-//colores 
-let objetivos =document.getElementById ("objetivos")
-let botoncolor = document.getElementById ("botoncolor")
-let colorines =document.getElementById ("color de jugadores")
-let colores = []
-function objetos (){
-
-  if (colores > cantidadJugadores || colores  <cantidadJugadores){
-    alert ("no coincide la cantidad de jugadores con los colores")
-  }
-  colores.push (objetivos.value)
-  console.log (colores)
-  
-}
-function final (){
-  while (i < players){
-  if (jugador === players[i]){
-      let pizzeria=document.createElement ("botoncolor")
-      pizzeria.textContent= players
-      objetivos.appendChild (p)}
-  i++
-
- }
-}
-colorines.addEventListener ("click",final)
-
-//intento de la eleccion de color
-//intento color2
